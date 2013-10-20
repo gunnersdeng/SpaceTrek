1fe544247a5c5e3043017330ec70be5a9c6389a8
yongke <huangyke@gmail.com>
2013-10-20 15:59:25 -0700

    looging scene

---
+++ SpaceTrek/LoadingScene.mm
@@ -0,0 +1,14 @@
+//
+//  LoadingScene.mm
+//  SpaceTrek
+//
+//  Created by huang yongke on 13-10-20.
+//  Copyright 2013年 huang yongke. All rights reserved.
+//
+
+#import "LoadingScene.h"
+
+
+@implementation LoadingScene
+
+@end
